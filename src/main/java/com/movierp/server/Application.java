b056a49097a60a2812a37af8a928be1de62360c7
César Alberca <cesalberca@gmail.com>
2016-12-17 15:09:51 +0100

    First commit

---
+++ src/main/java/com/movierp/server/Application.java
@@ -0,0 +1,14 @@
+package com.movierp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Cesar
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/movierp/server/User.java
@@ -0,0 +1,22 @@
+package com.movierp.server;
+
+/**
+ * Created by Cesar
+ */
+public class User {
+    private final long id;
+    private final String name;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
