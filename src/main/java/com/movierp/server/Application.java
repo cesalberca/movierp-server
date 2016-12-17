5365f33f62a65ddeee1626d35ddfd922e21af438
César Alberca <cesalberca@gmail.com>
2016-12-17 15:22:04 +0100

    Removed comments

--- src/main/java/com/movierp/server/Application.java
+++ src/main/java/com/movierp/server/Application.java
@@ -3,9 +3,6 @@ package com.movierp.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by Cesar
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
