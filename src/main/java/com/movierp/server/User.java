bb878b9d9429b9f4a22f9d7c17b8463d6bfde4dd
César Alberca <cesalberca@gmail.com>
2016-12-17 15:23:10 +0100

    Removed comments and removed id

--- src/main/java/com/movierp/server/User.java
+++ src/main/java/com/movierp/server/User.java
@@ -1,19 +1,10 @@
 package com.movierp.server;
 
-/**
- * Created by Cesar
- */
 public class User {
-    private final long id;
     private final String name;
 
-    public User(long id, String name) {
-        this.id = id;
+    public User(String name) {
         this.name = name;
-    }
-
-    public long getId() {
-        return id;
     }
 
     public String getName() {
--- src/main/java/com/movierp/server/UserController.java
+++ src/main/java/com/movierp/server/UserController.java
@@ -4,18 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-/**
- * Created by Cesar
- */
 @RestController
 public class UserController {
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/user")
     public User user(@RequestParam(value="name", defaultValue="World") String name) {
-        return new User(counter.incrementAndGet(), String.format(template, name));
+        return new User(String.format(template, name));
     }
 }
