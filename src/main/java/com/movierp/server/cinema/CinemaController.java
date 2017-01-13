25e19cb05efc8101bee9f540a812cfb37c7b5a07
César Alberca <cesalberca@gmail.com>
2017-01-13 13:07:53 +0100

    Added the correct annotations

--- src/main/java/com/movierp/server/cinema/CinemaController.java
+++ src/main/java/com/movierp/server/cinema/CinemaController.java
@@ -1,15 +1,13 @@
 package com.movierp.server.cinema;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/cinemas")
 public class CinemaController {
     private final CinemaDao cinemaDao;
@@ -21,20 +19,17 @@ public class CinemaController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
     public ResponseEntity create(@RequestBody Cinema cinema) {
         cinemaDao.save(cinema);
         return new ResponseEntity(cinema, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<Cinema> getCinemas() {
         return (List<Cinema>) cinemaDao.findAll();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public Cinema getCinema(@PathVariable("id") long id) {
         return cinemaDao.findOne(id);
     }
--- src/main/java/com/movierp/server/cinema/CinemaDao.java
+++ src/main/java/com/movierp/server/cinema/CinemaDao.java
@@ -2,9 +2,16 @@ package com.movierp.server.cinema;
 
 import javax.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Transactional
-@Repository
+@RepositoryRestResource(path = "cinemas")
 public interface CinemaDao extends CrudRepository<Cinema, Long> {
+    @RestResource(path = "names", rel ="name")
+    List<Cinema> findByName(String name);
 }
