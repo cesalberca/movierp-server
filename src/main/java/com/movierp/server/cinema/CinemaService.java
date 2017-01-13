020fc2962204e80f3000066975da60c65c8e646d
cesalberca <cesalberca@gmail.com>
2017-01-13 18:47:39 +0100

    Renamed from CinemaDao to CinemaService

--- src/main/java/com/movierp/server/cinema/CinemaService.java
+++ src/main/java/com/movierp/server/cinema/CinemaService.java
@@ -2,16 +2,15 @@ package com.movierp.server.cinema;
 
 import javax.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Transactional
 @RepositoryRestResource(path = "cinemas")
-public interface CinemaDao extends CrudRepository<Cinema, Long> {
-    @RestResource(path = "names", rel ="name")
-    List<Cinema> findByName(String name);
+public interface CinemaService extends CrudRepository<Cinema, Long> {
+    @RestResource(path = "findByName")
+    List<Cinema> findByName(@Param("name") String name);
 }
