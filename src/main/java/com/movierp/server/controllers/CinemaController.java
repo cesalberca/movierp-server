a6a2d77892c98453ac3bc753e6b291ba7d880c07
César Alberca <cesalberca@gmail.com>
2017-01-18 00:17:24 +0100

    Moved services and controllers to it's own packages

--- src/main/java/com/movierp/server/controllers/CinemaController.java
+++ src/main/java/com/movierp/server/controllers/CinemaController.java
@@ -1,5 +1,7 @@
-package com.movierp.server.cinema;
+package com.movierp.server.controllers;
 
+import com.movierp.server.entities.CinesEntity;
+import com.movierp.server.services.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -7,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cinemas")
@@ -20,21 +21,21 @@ public class CinemaController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Cinema> create(Cinema cinema) {
+    public ResponseEntity<CinesEntity> create(CinesEntity cinema) {
         this.cinemaService.save(cinema);
         HttpHeaders responseHeaders = new HttpHeaders();
         return new ResponseEntity<>(cinema, responseHeaders, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public Collection<Cinema> findAll() {
-        return (Collection<Cinema>) this.cinemaService.findAll();
+    public Collection<CinesEntity> findAll() {
+        return (Collection<CinesEntity>) this.cinemaService.findAll();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity findOne(@PathVariable("id") long id) {
         try {
-            Cinema cinema = this.cinemaService.findOne(id);
+            CinesEntity cinema = this.cinemaService.findOne(id);
             return new ResponseEntity<>(cinema, HttpStatus.OK);
         } catch (Exception ex) {
             return new ResponseEntity<>(ex, HttpStatus.OK);
@@ -42,7 +43,7 @@ public class CinemaController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Cinema> update(@PathVariable("id") Cinema cinema) {
+    public ResponseEntity<CinesEntity> update(@PathVariable("id") CinesEntity cinema) {
         this.cinemaService.save(cinema);
         HttpHeaders responseHeaders = new HttpHeaders();
         return new ResponseEntity<>(cinema, responseHeaders, HttpStatus.OK);
