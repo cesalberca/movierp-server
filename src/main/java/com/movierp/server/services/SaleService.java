4f67afb671606881a3a02946198de3dfe8effa23
Vicente <vrs1985@hotmail.com>
2017-01-23 18:15:40 +0100

    Created SaleService

---
+++ src/main/java/com/movierp/server/services/SaleService.java
@@ -0,0 +1,16 @@
+package com.movierp.server.services;
+
+
+import com.movierp.server.entities.ClientesEntity;
+import com.movierp.server.entities.VentasEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+@RepositoryRestResource(collectionResourceRel = "sales", path = "sales")
+public interface SaleService extends CrudRepository<VentasEntity, Long> {
+}
