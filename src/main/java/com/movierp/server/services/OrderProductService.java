97a8d765a21eeb972f3ebe78cdcf89f97979252e
Vicente <vrs1985@hotmail.com>
2017-01-23 18:19:05 +0100

    Created OrderProductService comentado para evitar los errores

--- src/main/java/com/movierp/server/services/OrderProductService.java
+++ src/main/java/com/movierp/server/services/OrderProductService.java
@@ -8,9 +8,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-
+/*
 @Transactional
 @Repository
 @RepositoryRestResource(collectionResourceRel = "ordersProducts", path = "ordersProducts")
 public interface OrderProductService extends CrudRepository<PedProdEntity>, Long> {
-}
+}*/
