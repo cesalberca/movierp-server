48f652dc5d25c08ea08c0e76d162f20b90a75c30
César Alberca <cesalberca@gmail.com>
2017-01-23 19:05:21 +0100

    Created relations between Users and Employees

--- src/main/java/com/movierp/server/entities/UsuariosEntity.java
+++ src/main/java/com/movierp/server/entities/UsuariosEntity.java
@@ -2,21 +2,38 @@ package com.movierp.server.entities;
 
 import javax.persistence.*;
 
-/**
- * Created by Cesar
- */
 @Entity
-@Table(name = "usuarios", schema = "movierp", catalog = "")
+@Table(name = "usuarios", schema = "movierp")
 public class UsuariosEntity {
-    private long idUsuario;
-    private String rol;
-    private String nombre;
-    private String contrasena;
-    private Long idEmpleado;
-    private EmpleadosEntity empleadosByIdEmpleado;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_usuario", nullable = false)
+    private long idUsuario;
+
+    @Column(name = "rol")
+    private String rol;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "contrasena")
+    private String contrasena;
+
+    @OneToOne
+    @JoinColumn(name = "id_empleado")
+    private EmpleadosEntity empleadosEntity;
+
+    public UsuariosEntity() {
+    }
+
+    public UsuariosEntity(String rol, String nombre, String contrasena, EmpleadosEntity empleadosEntity) {
+        this.rol = rol;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.empleadosEntity = empleadosEntity;
+    }
+
     public long getIdUsuario() {
         return idUsuario;
     }
@@ -25,8 +42,6 @@ public class UsuariosEntity {
         this.idUsuario = idUsuario;
     }
 
-    @Basic
-    @Column(name = "rol", nullable = true, length = 50)
     public String getRol() {
         return rol;
     }
@@ -35,8 +50,6 @@ public class UsuariosEntity {
         this.rol = rol;
     }
 
-    @Basic
-    @Column(name = "nombre", nullable = true, length = 50)
     public String getNombre() {
         return nombre;
     }
@@ -45,8 +58,6 @@ public class UsuariosEntity {
         this.nombre = nombre;
     }
 
-    @Basic
-    @Column(name = "contrasena", nullable = true, length = 50)
     public String getContrasena() {
         return contrasena;
     }
@@ -55,49 +66,11 @@ public class UsuariosEntity {
         this.contrasena = contrasena;
     }
 
-    @Basic
-    @Column(name = "id_empleado", nullable = true)
-    public Long getIdEmpleado() {
-        return idEmpleado;
+    public EmpleadosEntity getEmpleadosEntity() {
+        return empleadosEntity;
     }
 
-    public void setIdEmpleado(Long idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UsuariosEntity that = (UsuariosEntity) o;
-
-        if (idUsuario != that.idUsuario) return false;
-        if (rol != null ? !rol.equals(that.rol) : that.rol != null) return false;
-        if (nombre != null ? !nombre.equals(that.nombre) : that.nombre != null) return false;
-        if (contrasena != null ? !contrasena.equals(that.contrasena) : that.contrasena != null) return false;
-        if (idEmpleado != null ? !idEmpleado.equals(that.idEmpleado) : that.idEmpleado != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (idUsuario ^ (idUsuario >>> 32));
-        result = 31 * result + (rol != null ? rol.hashCode() : 0);
-        result = 31 * result + (nombre != null ? nombre.hashCode() : 0);
-        result = 31 * result + (contrasena != null ? contrasena.hashCode() : 0);
-        result = 31 * result + (idEmpleado != null ? idEmpleado.hashCode() : 0);
-        return result;
-    }
-
-    @ManyToOne
-    @JoinColumn(name = "id_empleado", referencedColumnName = "id_empleado", insertable = false, updatable = false)
-    public EmpleadosEntity getEmpleadosByIdEmpleado() {
-        return empleadosByIdEmpleado;
-    }
-
-    public void setEmpleadosByIdEmpleado(EmpleadosEntity empleadosByIdEmpleado) {
-        this.empleadosByIdEmpleado = empleadosByIdEmpleado;
+    public void setEmpleadosEntity(EmpleadosEntity empleadosEntity) {
+        this.empleadosEntity = empleadosEntity;
     }
 }
