bc759ee48a779bc4fde8647ae1aa5f7e0ff3299f
cesalberca <cesalberca@gmail.com>
2017-01-23 21:09:49 +0100

    More relations

--- src/main/java/com/movierp/server/services/OrderProductService.java
+++ src/main/java/com/movierp/server/services/OrderProductService.java
@@ -1,13 +1,6 @@
 package com.movierp.server.services;
 
 
-import com.movierp.server.entities.ClientesEntity;
-import com.movierp.server.entities.PedProdEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 /*
 @Transactional
 @Repository
