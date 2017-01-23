59200a08e3d5ea5f57438d520b8495e6b0a8588b
César Alberca <cesalberca@gmail.com>
2017-01-23 12:46:29 +0100

    Created employee api endpoint

---
+++ src/main/java/com/movierp/server/services/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.movierp.server.services;
+
+import com.movierp.server.entities.EmpleadosEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
+public interface EmployeeService extends CrudRepository<EmpleadosEntity, Long> {
+}
