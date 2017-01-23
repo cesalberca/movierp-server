b0de2af9e9cd92335d0ec6ab2751364cafb16d83
Vicente <vrs1985@hotmail.com>
2017-01-23 17:58:17 +0100

    Created orders services

---
+++ src/main/java/com/movierp/server/services/OrderService.java
@@ -0,0 +1,14 @@
+package com.movierp.server.services;
+
+import com.movierp.server.entities.PedidosEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+@RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
+public interface OrderService extends CrudRepository<PedidosEntity, Long> {
+}
