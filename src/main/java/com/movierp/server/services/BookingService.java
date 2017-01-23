d7019a22d1ea42c50f2d874f7e596ec22765ef95
Vicente <vrs1985@hotmail.com>
2017-01-23 18:10:45 +0100

    Created RoomService

---
+++ src/main/java/com/movierp/server/services/BookingService.java
@@ -0,0 +1,14 @@
+package com.movierp.server.services;
+
+import com.movierp.server.entities.ReservasEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+@RepositoryRestResource(collectionResourceRel = "booking", path = "booking")
+public interface BookingService extends CrudRepository<ReservasEntity, Long> {
+}
