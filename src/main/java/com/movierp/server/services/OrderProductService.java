5394014c7f94a3bc8c4d208b913363c08a159a91
César Alberca <cesalberca@gmail.com>
2017-01-24 16:31:58 +0100

    Deleted order product

--- src/main/java/com/movierp/server/services/OrderProductService.java
+++
@@ -1,9 +0,0 @@
-package com.movierp.server.services;
-
-
-/*
-@Transactional
-@Repository
-@RepositoryRestResource(collectionResourceRel = "ordersProducts", path = "ordersProducts")
-public interface OrderProductService extends CrudRepository<PedProdEntity>, Long> {
-}*/
