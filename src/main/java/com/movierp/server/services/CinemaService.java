80803bada87943c04488f150d90fe8162a7d0928
César Alberca <cesalberca@gmail.com>
2017-02-15 18:15:10 +0100

    Removed projection

--- src/main/java/com/movierp/server/services/CinemaService.java
+++ src/main/java/com/movierp/server/services/CinemaService.java
@@ -1,7 +1,6 @@
 package com.movierp.server.services;
 
 import com.movierp.server.entities.Cinema;
-import com.movierp.server.projections.InlineCinemaRoom;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,7 +8,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "cinemas", path = "cinemas", excerptProjection = InlineCinemaRoom.class)
+@RepositoryRestResource(collectionResourceRel = "cinemas", path = "cinemas")
 public interface CinemaService extends CrudRepository<Cinema, Long> {
     @RestResource(path = "findByNombre")
     List<Cinema> findByNombre(@Param("nombre") String name);
