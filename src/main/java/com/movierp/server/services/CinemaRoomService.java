7ef9bdf963ff169e502356d7ceae8c76833babd0
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-02-20 21:00:03 +0100

    Trying to add complex select @Query support

--- src/main/java/com/movierp/server/services/CinemaRoomService.java
+++ src/main/java/com/movierp/server/services/CinemaRoomService.java
@@ -1,9 +1,17 @@
 package com.movierp.server.services;
 
+import com.movierp.server.entities.Cinema;
 import com.movierp.server.entities.CinemaRoom;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "rooms", path = "rooms")
 public interface CinemaRoomService extends CrudRepository<CinemaRoom, Long> {
+    @RestResource(path = "findByIdCine")
+    List<CinemaRoom> findByIdCine(@Param("idCine") long idCine);
 }
--- src/main/java/com/movierp/server/services/CinemaService.java
+++ src/main/java/com/movierp/server/services/CinemaService.java
@@ -1,7 +1,11 @@
 package com.movierp.server.services;
 
 import com.movierp.server.entities.Cinema;
+import com.movierp.server.entities.CinemaRoom;
+import com.movierp.server.entities.Session;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -12,4 +16,19 @@ import java.util.List;
 public interface CinemaService extends CrudRepository<Cinema, Long> {
     @RestResource(path = "findByNombre")
     List<Cinema> findByNombre(@Param("nombre") String name);
+
+    //@Query("select s from Session s where s.id_sala in (select id_sala from CinemaRoom where id_cine = + ?)")
+    //List<Session> findSessionsByCinemaId(@Param("cinemaId") int cinemaId);
+
+    @RestResource(path = "findCinemas")
+    @Query("select c from Cinema c where c.idCine = 44")
+    List<Cinema> findCinemas(@Param("idCine") long idCine);
+
+    @Query("select c from CinemaRoom c")
+    List<CinemaRoom> findCinemaRoom();
+
+    // Comentario para César:
+    // Lo que estaría bien es que :
+    // 1 - Le paso la id del Cine
+    // 2 - Devuelve todas las sesiones del Cine con esa id
 }
