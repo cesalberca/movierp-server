e4380afe45472d7392f57db6e935575b3ffa4489
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-02-27 19:26:02 +0100

    Added buyTicketsForSessionById method to Session

--- src/main/java/com/movierp/server/services/SessionService.java
+++ src/main/java/com/movierp/server/services/SessionService.java
@@ -2,9 +2,23 @@ package com.movierp.server.services;
 
 
 import com.movierp.server.entities.Session;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource(collectionResourceRel = "sessions", path = "sessions")
 public interface SessionService  extends CrudRepository<Session, Long>{
+
+    /**
+     * Updates the available seats of a given session
+     * @param idSesion the session to be updated
+     * @param ticketAmount the amount of available seats to be subtracted
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Session s set s.butacasLibres = s.butacasLibres - :ticketAmount where s.idSesion = :idSesion and s.butacasLibres - :ticketAmount >= 0")
+    void buyTicketsForSessionById(@Param("idSesion") long idSesion, @Param("ticketAmount") int ticketAmount);
 }
