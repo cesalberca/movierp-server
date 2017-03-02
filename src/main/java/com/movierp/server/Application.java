775db9f7d8dca247dc5a03bfffe88a99bdf3e621
César Alberca <cesalberca@gmail.com>
2017-03-02 22:42:35 +0100

    Now deploys war

--- src/main/java/com/movierp/server/Application.java
+++ src/main/java/com/movierp/server/Application.java
@@ -2,9 +2,10 @@ package com.movierp.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
